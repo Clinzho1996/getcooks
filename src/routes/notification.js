fd3c6d7def886b315ed140f2df5fc93272a8b8d9
clinzho1996 <confidinho@yahoo.com>
2026-09-23 21:22:14 +0100

    mark read

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -19,7 +19,7 @@ router.use(protect);
 
 // User routes
 router.get("/", getNotifications);
-router.put("/all-read", markAllAsRead);
+router.patch("/all-read", markAllAsRead);
 router.post("/push-token", registerPushToken);
 router.delete("/push-token", unregisterPushToken);
 router.get("/settings", getNotificationSettings);
